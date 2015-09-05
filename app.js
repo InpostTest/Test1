38c850962463c02da26d143527dd46ad2db4465a
Coffeephile <coffeephile@live.com>
2015-09-05 04:08:01 +0200

    XhrUtils class for REST requests

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
+//ToDo: JSDOCS, CACHE
+
 //logger type enum
 var LoggerTypes;
 (function (LoggerTypes) {
     LoggerTypes[LoggerTypes["console"] = 0] = "console";
 })(LoggerTypes || (LoggerTypes = {}));
 
-//logger class
+//logger service class
 var Logger = (function () {
 
     //constructor
@@ -24,6 +26,30 @@ var Logger = (function () {
     return Logger;
 })();
 
+//xhr utils service class
+var XhrUtils = (function () {
+
+    //constructor
+    function XhrUtils() {
+    }
+
+    XhrUtils.prototype.get = function (url, loadCallback, errorCallback) {
+        
+        var req = new XMLHttpRequest();
+        req.callback = loadCallback;
+        req.onload = function() {
+            if (this.readyState === 4)
+                  if (this.status === 200) 
+                        this.callback(JSON.parse(this.responseText));
+            };
+        req.onerror = errorCallback;
+        req.open("GET", url, true);
+        req.send(null);
+    };
+
+    return XhrUtils;
+})();
+
 //list viewer class
 var ListViewer = (function () {
 	
@@ -39,28 +65,18 @@ var ListViewer = (function () {
 
     ListViewer.prototype.loadData = function () {
 
-        var me = this;
+        this.xhrService.get(this.restURI, this.loadSuccess.bind(this), this.loadError.bind(this));
+    };
 
-        loadFile(this.restURI);
+    ListViewer.prototype.loadSuccess = function (data) {
 
-        function xhrSuccess () { 
-            
-            me.data = JSON.parse(this.responseText)._embedded.machines;
-            me.render.call(me);
-        }
+        this.data = data._embedded.machines;
+        this.render.call(this);
+    };
 
-        function xhrError () { 
-        }
+    ListViewer.prototype.loadError = function (xhr) {
 
-        function loadFile (url, callback) {
-
-          var req = new XMLHttpRequest();
-          req.callback = callback;
-          req.onload = xhrSuccess;
-          req.onerror = xhrError;
-          req.open("get", url, true);
-          req.send(null);
-        }
+        this.loggerService.log("ERROR");
     };
 
     ListViewer.prototype.render = function () {
@@ -92,12 +108,14 @@ window.onload = function () {
     var mainLogger = new Logger(LoggerTypes.console);
     mainLogger.log("SDK INITIALIZE");
 
+    var xhrUtils = new XhrUtils();
+
     var listConfig = {
         restURI: "https://api-pl.easypack24.net/v4/machines?type=0",
         el: document.getElementById('content')
-    }
+    };
 
-    var listViewer = new ListViewer(listConfig.el, listConfig.restURI, mainLogger);
+    var listViewer = new ListViewer(listConfig.el, listConfig.restURI, mainLogger, xhrUtils);
 
     listViewer.loadData();
 };
