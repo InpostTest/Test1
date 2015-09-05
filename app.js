8fa82f682c7520bbf647d2f4ec5ae827e94cafec
Coffeephile <coffeephile@live.com>
2015-09-05 02:11:57 +0200

    logger class

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
+
+//logger type enum
+var LoggerTypes;
+(function (LoggerTypes) {
+    LoggerTypes[LoggerTypes["console"] = 0] = "console";
+})(LoggerTypes || (LoggerTypes = {}));
+
+//logger class
+var Logger = (function () {
+
+    //constructor
+    function Logger(loggerType) {
+        this.logType = loggerType;
+    }
+
+    Logger.prototype.log = function (data) {
+        switch (this.logType) {
+            case LoggerTypes.console:
+                console.log(data);
+                break;
+        }
+    };
+
+    return Logger;
+})();
+
+//list viewer class
 var ListViewer = (function () {
 	
 	//constructor
-    function ListViewer(element) {
+    function ListViewer(element, logger) {
         this.element = element;
+        this.logger = logger;
+        this.logger.log("SDK INITIALIZE");
     }
 	
     return ListViewer;
@@ -11,5 +40,5 @@ var ListViewer = (function () {
 window.onload = function () {
 	
     var el = document.getElementById('content');
-    var listViewer = new ListViewer(el);
+    var listViewer = new ListViewer(el, new Logger(LoggerTypes.console));
 };
